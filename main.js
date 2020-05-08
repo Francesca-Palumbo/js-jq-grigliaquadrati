5e87c19fe62fd0f3fcd2535d734af099c0a3c6b6
Francesca Palumbo <econofra87@gmail.com>
2020-05-08 15:34:24 +0200

    pari ok

--- main.js
+++ main.js
@@ -4,20 +4,28 @@
 // BONUS 2: se il numero da inserire è pari scriverlo in rosso, se è dispari in nero, se è 0 in verde
 
 $('.quadrato').each(function(){
-    var numero_random = genera_random(1, 10);
+    var numero_random = generaRandom(0, 10);
+    console.log(numero_random);
+    $(this).text(numero_random);
 
-    if (numero_random % 2 == 0) {
-        $(this).addClass('rosso');
-    } else if ((numero_random==0)) {
+    if (numero_random == 0) {
         $(this).addClass('verde');
-    }else {
+    } else if (numero_random % 2 == 0) {
+        $(this).addClass('rosso')
+    } else {
         $(this).addClass('nero');
     }
+    // if (numero_random % 2 == 0) {
+    //     $(this).addClass('rosso');
+    // } else if ((numero_random==0)) {
+    //     $(this).addClass('verde');
+    // }else {
+    //     $(this).addClass('nero');
+    // }
 });
 
 
-
-
-function genera_random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+function generaRandom(min, max) {
+    var numero_casuale = Math.floor(Math.random() * (max - min + 1) ) + min;
+    return numero_casuale;
 }
